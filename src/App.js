61e141c17db07f125fe6c70e017af20d042c9b75
LovarajDivili <lovarajudivili1668@gmail.com>
2022-05-28 12:06:07 +0000

    main

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import './App.css'
-// import Counter from './components/Counter'
-import {useState} from 'react'
+
 
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
-    return(
-        <div className="container">
-            <h1>Hello...! EveryOne</h1>
-            <p>Click the below buttons.                 
-            <br/> And <br/> Increasing the Counter value.                
-            <br/>Decreasing the counter value. <br/> HURRYUP.....!</p>
-            <h2>{counter}</h2>
-            <div className="b_container">
-                <button
-            onClick={() => { setCounter (counter + 1) }}
-            >
-            Increase Counter</button>
-            <button className="setCounter"
-            onClick={() => { setCounter (counter - 1) }}
-            >
-            Decrease Counter</button>
-            </div>
-        </div>
+    return (
+        <h1>Lovaraju</h1>
     )
 }
 
